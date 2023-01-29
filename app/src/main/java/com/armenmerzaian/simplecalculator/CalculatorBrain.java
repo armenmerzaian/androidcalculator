df64a55f91f0f45d2ac00c73b36d85bf8c43ebd4
armenmerzaian <71707172+armenmerzaian@users.noreply.github.com>
2023-01-28 22:37:28 -0500

    everything other than calculation works

--- app/src/main/java/com/armenmerzaian/simplecalculator/CalculatorBrain.java
+++ app/src/main/java/com/armenmerzaian/simplecalculator/CalculatorBrain.java
@@ -1,5 +1,112 @@
 package com.armenmerzaian.simplecalculator;
 
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CalculatorBrain {
 
+    private static final int MAXSIZE = 16;
+    //initialize member variables
+    private static final ArrayList<String> m_inputValues = new ArrayList<>();
+    private static Number m_outputValue = 0;
+
+    public static void resetAll(){
+        m_inputValues.clear();
+        m_outputValue = 0;
+    }
+
+    public static void popBack(){
+        if(!m_inputValues.isEmpty()){
+            m_inputValues.remove(m_inputValues.size() - 1);
+        }
+    }
+
+    private static void setOutputValue(Number val){
+        m_outputValue = val;
+    }
+
+    public static String getInputValues() {
+        StringBuilder composite = new StringBuilder();
+        for(String a : m_inputValues){
+            if(a.equals("%") || a.equals("÷") || a.equals("x") || a.equals("-") || a.equals("+")){
+                composite.append(" ").append(a).append(" ");
+            } else {
+                composite.append(a);
+            }
+        }
+        return composite.toString();
+    }
+
+    public static String getLastInput(){
+        if(!m_inputValues.isEmpty()){
+            return m_inputValues.get(m_inputValues.size()-1);
+        }
+        return "";
+    }
+
+    public static String getOutputValue() {
+        return m_outputValue.toString();
+    }
+
+    public static void flipSign(){
+        if(!m_inputValues.isEmpty()){
+            int lastIndex;
+            // Starting from the last index of the list
+            for (lastIndex = m_inputValues.size()-1; lastIndex>=0; lastIndex--) {
+                if (m_inputValues.get(lastIndex).matches("[+\\-x÷%]")) { // matches one of ops
+                    break;
+                }
+            }
+            lastIndex++;
+            if(m_inputValues.get(lastIndex).matches("^-\\d+$")){ //matches '-' at the beginning of the string
+                m_inputValues.set(lastIndex, m_inputValues.get(lastIndex).substring(1));
+            } else {
+                m_inputValues.set(lastIndex, "-" + m_inputValues.get(lastIndex));
+            }
+        }
+    }
+
+    public static void pushValue(String val){
+        if (m_inputValues.size() < MAXSIZE){
+            m_inputValues.add(val);
+        }
+    }
+
+    public static void calculate(){
+        //TODO: Calculator logic
+        // mainActivity pushes user inputs to m_inputValues array
+        // this function should parse the array and perform the calculation
+        // once finished call setOutputValue(value) with the calculated value
+        String operatorRegex = "[+\\-x÷%/]";
+        Pattern operatorPattern = Pattern.compile(operatorRegex);
+        ArrayList<String> numbers = new ArrayList<>();
+        ArrayList<String> operators = new ArrayList<>();
+        for (int i = 0; i < m_inputValues.size(); i++) {
+            String current = m_inputValues.get(i);
+            Matcher operatorMatcher = operatorPattern.matcher(current);
+            if (operatorMatcher.matches()) {
+                operators.add(current);
+            } else {
+                numbers.add(current);
+            }
+        }
+        double currentNumber = Double.parseDouble(numbers.get(0));
+        for (int i = 0; i < operators.size(); i++) {
+            String operator = operators.get(i);
+            double nextNumber = Double.parseDouble(numbers.get(i + 1));
+            if (operator.equals("+")) {
+                currentNumber += nextNumber;
+            } else if (operator.equals("-")) {
+                currentNumber -= nextNumber;
+            } else if (operator.equals("x")) {
+                currentNumber *= nextNumber;
+            } else if (operator.equals("÷")) {
+                currentNumber /= nextNumber;
+            } else if (operator.equals("%")) {
+                currentNumber %= nextNumber;
+            }
+        }
+        setOutputValue(currentNumber);
+    }
 }
--- app/src/main/java/com/armenmerzaian/simplecalculator/MainActivity.java
+++ app/src/main/java/com/armenmerzaian/simplecalculator/MainActivity.java
@@ -1,14 +1,180 @@
 package com.armenmerzaian.simplecalculator;
 
+import androidx.annotation.StringDef;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class MainActivity extends AppCompatActivity{
+
+    TextView tv_Input;
+    TextView tv_Result;
+
+    Button btn_Clear;
+    Button btn_PlusMinus;
+    Button btn_Percent;
+    Button btn_Divide;
+
+    Button btn_Seven;
+    Button btn_Eight;
+    Button btn_Nine;
+    Button btn_Multiply;
+
+    Button btn_Four;
+    Button btn_Five;
+    Button btn_Six;
+    Button btn_Minus;
+
+    Button btn_One;
+    Button btn_Two;
+    Button btn_Three;
+    Button btn_Plus;
+
+    Button btn_Zero;
+    Button btn_Decimal;
+    Button btn_Equals;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initialize everything
+        initCalculator();
     }
+
+    public void initCalculator(){
+        //init calculator brain
+        //initialize the widgets
+        tv_Input = (TextView) findViewById(R.id.calcInput);
+        tv_Result = (TextView) findViewById(R.id.calcResult);
+        resetTextViews();
+
+        btn_Clear = (Button) findViewById(R.id.buttonClear);
+        btn_PlusMinus = (Button) findViewById(R.id.buttonPlusMinus);
+        btn_Percent = (Button) findViewById(R.id.buttonPercent);
+        btn_Divide = (Button) findViewById(R.id.buttonDivide);
+
+        btn_Seven = (Button) findViewById(R.id.buttonSeven);
+        btn_Eight = (Button) findViewById(R.id.buttonEight);
+        btn_Nine = (Button) findViewById(R.id.buttonNine);
+        btn_Multiply = (Button) findViewById(R.id.buttonMultiply);
+
+        btn_Four = (Button) findViewById(R.id.buttonFour);
+        btn_Five = (Button) findViewById(R.id.buttonFive);
+        btn_Six = (Button) findViewById(R.id.buttonSix);
+        btn_Minus = (Button) findViewById(R.id.buttonMinus);
+
+
+        btn_One = (Button) findViewById(R.id.buttonOne);
+        btn_Two = (Button) findViewById(R.id.buttonTwo);
+        btn_Three = (Button) findViewById(R.id.buttonThree);
+        btn_Plus = (Button) findViewById(R.id.buttonPlus);
+
+        btn_Zero = (Button) findViewById(R.id.buttonZero);
+        btn_Decimal = (Button) findViewById(R.id.buttonDecimal);
+        btn_Equals = (Button) findViewById(R.id.buttonEquals);
+
+        //add the button widgets to an array for better developer experience
+        Collection<View> widgetListNums = new ArrayList<View>();
+        Collection<View> widgetListOps = new ArrayList<View>();
+        Collections.addAll(widgetListNums,
+                btn_Seven, btn_Eight, btn_Nine,
+                btn_Four, btn_Five, btn_Six,
+                btn_One, btn_Two, btn_Three,
+                btn_Zero);
+        Collections.addAll(widgetListOps,
+                btn_Percent, btn_Divide, btn_Multiply, btn_Minus, btn_Plus, btn_Decimal);
+
+        //add onClick listeners to each NUMBER widget
+        widgetListNums.forEach(widget -> widget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CalculatorBrain.pushValue(((TextView)v).getText().toString());
+                refreshInput();
+            }
+        }));
+
+        // add onClick listeners to each OPERATION widget
+        widgetListOps.forEach(widget -> widget.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if(CalculatorBrain.getLastInput().matches("[^%÷x\\-+.]")) { //anything that's not %÷x-+
+                    CalculatorBrain.pushValue(((TextView)v).getText().toString());
+                    refreshInput();
+                }
+            }
+        }));
+
+        btn_PlusMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*if(CalculatorBrain.getLastInput().matches("^-?\\d+$")){ // matches any pos or neg integer
+                    CalculatorBrain.flipSign();
+                    refreshInput();
+                }*/
+                if(!CalculatorBrain.getInputValues().isEmpty()){
+                    CalculatorBrain.flipSign();
+                    refreshInput();
+                }
+            }
+        });
+
+        btn_Clear.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                resetTextViews();
+                return true;
+            }
+        });
+
+        btn_Clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!CalculatorBrain.getInputValues().isEmpty()) {
+                    CalculatorBrain.popBack();
+                    refreshInput();
+                }
+            }
+        });
+
+        btn_Equals.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(CalculatorBrain.getLastInput().matches("[^%÷x\\-+.]")) { //anything that's not %÷x-+
+                    CalculatorBrain.calculate();
+                    refreshOutput();
+                }
+            }
+        });
+    }
+
+    public void refreshInput(){
+        tv_Input.setText(CalculatorBrain.getInputValues());
+    }
+
+    public void refreshOutput(){
+        tv_Result.setText(CalculatorBrain.getOutputValue());
+    }
+
+    private void resetTextViews(){
+        CalculatorBrain.resetAll();
+        tv_Input.setText("");
+        tv_Result.setText(CalculatorBrain.getOutputValue().toString());
+    }
+
+
+
+
+
+
 }
